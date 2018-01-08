6022a297b3f4c4f65591f4f56cb6784041adc37e
dswhit <dswhit@node019.cm.cluster>
2018-01-08 15:12:11 -0500

    now recognizes sequence-inline barcodes and index values

--- 01_sort2Barcodes.py
+++ 01_sort2Barcodes.py
@@ -1,24 +1,28 @@
 #!~/anaconda3/envs/py2/bin/python
 
 from Bio import SeqIO
-import sys 
+import sys
+import itertools 
 
 def main():
-
+    #Updated for corrected file paths by dw
     #Setup variables (could parse command line args instead)
-    file_f = "/home/vxue/data/experimental/SORTCERY/2016_11_09/161011Kea_D16-11808_1_sequence.fastq"
-    file_r = "/home/vxue/data/experimental/SORTCERY/2016_11_09/161011Kea_D16-11808_2_sequence.fastq"
-    dir_out = "/home/vxue/data/experimental/SORTCERY/2016_11_09/workspace/"
+    file_f = "/scratch/users/dswhit/DATA/titeseq_illumina/171220Kea_D17-10768_1_sequence.fastq"
+    file_r = "/scratch/users/dswhit/DATA/titeseq_illumina/171220Kea_D17-10768_2_sequence.fastq"
+#    file_f = "/scratch/users/dswhit/DATA/titeseq_illumina/test_1.fastq"
+#    file_r = "/scratch/users/dswhit/DATA/titeseq_illumina/test_2.fastq"
+
+
+    dir_out = "/scratch/users/dswhit/DATA/titeseq_illumina/workspace/"
     myFormat = "fastq-sanger"
-    numBarcodes = (24*7) #seventh is for naive pool
+    numBarcodes = (24*9) #1 is enrich sorts and controls, 2-9 is four TiteSeq runs
     
     records_f = SeqIO.parse(open(file_f,"rU"), myFormat)
     records_r = SeqIO.parse(open(file_r,"rU"), myFormat)
-    
     myOpenOutStreams = openOutStreams(dir_out,numBarcodes)
-        
-    for (forward, reverse) in zip(records_f,records_r):   
-        barcodeIndex = getBarcode(forward)
+    for (forward, reverse) in itertools.izip(records_f,records_r):   
+#    for (forward, reverse) in zip(records_f,records_r):
+        barcodeIndex = getBarcode(forward, reverse)
         if(barcodeIndex>-1):
             #print(barcodeIndex, forward.seq ,reverse.seq)
             SeqIO.write(forward, myOpenOutStreams[barcodeIndex], myFormat)
@@ -31,6 +35,7 @@ def main():
 def openOutStreams(dir_out,num):
     myArray = []
     for i in range(num):
+        print str(i)
         myArray.append(open(dir_out+"barcode_"+str(i),'w'))
     return myArray
 
@@ -38,16 +43,25 @@ def closeOutStreams(array):
     for each in array:
         each.close()
             
-def getBarcode(forward):
+def getBarcode(forward, reverse):
     sequence = str(forward.seq)
     forward_Quality = forward.letter_annotations['phred_quality']
    
     #First check if barcode2 is available
     #Barcode2 = index (identifies which experiment)
-    myBarcodes2 = ["ATCACG","ACAGTG","CGATGT","CAGATC","GATCAG","GCCAAT","TTAGGC"]
+    # Updated for 9 index sequences by dw
+    myBarcodes2 = ["ATCACG","CGATGT","TTAGGC","TGACCA","ACAGTG","GCCAAT",
+                    "CAGATC","ACTTGA","GATCAG"]
     myBarcodes2Index=-1
     
-    myBarcodes2Index = getClosestBarcode(forward.id[-8:],myBarcodes2,5)  # 5 indicates score must be better than 5
+    #Trying to get the index from the rev complement of the start of the reverse read
+    #Casting as a string removes the ability to use further features, obviously, try 
+    #to do in reverse order
+    # This worked, far as I can tell.
+    rev_sequence = (reverse.seq)
+    lastSix = rev_sequence[0:6]
+    rv_lastSix = lastSix.reverse_complement()
+    myBarcodes2Index = getClosestBarcode(rv_lastSix,myBarcodes2,5)  # 5 indicates score must be better than 5
     if(myBarcodes2Index==-1):
         return -1
     #Then check barcode1 quality
