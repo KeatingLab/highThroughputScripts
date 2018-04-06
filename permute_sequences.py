498e2852e9a3cebf4e5cb5953578f5c3928b586e
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2018-04-06 17:57:20 -0400

    Simple pair of scripts to score all sequences satisfying criteria and write them to file or console output

--- permute_sequences.py
+++ permute_sequences.py
@@ -2,7 +2,7 @@ import argparse
 import random
 import os
 
-amino_acids = "ARNDCEQGHILKMFPSTWYV"
+amino_acids = "ACEDGFIHKMLNQPSRTWVY"
 bases = "AGCT"
 
 def detect_language(template):
@@ -13,54 +13,58 @@ def detect_language(template):
         elif char not in bases and char in amino_acids:
             print("Inferring protein.")
             return amino_acids
+        elif len(char) > 1:
+            try:
+                # This is a set of allowed characters, so check each character individually
+                return detect_language(char)
+            except AssertionError:
+                pass
     assert False, "Can't detect whether to use amino acids or nucleotides"
 
-def write_permutations_recursive(template, base, language, outfiles):
+def all_permutations_recursive(template, base, language):
     if len(template) == 0:
-        outfiles[0].write(base + '\n')
+        yield base
     elif template[0] == '*':
         for i, char in enumerate(language):
             if len(base) == 0:
                 print("{} / {} first positions".format(i, len(language)))
-            if len(base) == 1:
-                outfile = outfiles[i % len(outfiles)]
-                write_permutations_recursive(template[1:], base + char, language, [outfile])
-            else:
-                write_permutations_recursive(template[1:], base + char, language, outfiles)
+            yield from all_permutations_recursive(template[1:], base + char, language)
+    elif len(template[0]) > 1:
+        # This item in the template contains a list of possible values
+        for i, char in enumerate(template[0]):
+            yield from all_permutations_recursive(template[1:], base + char, language)
     else:
-        write_permutations_recursive(template[1:], base + template[0], language, outfiles)
+        yield from all_permutations_recursive(template[1:], base + template[0], language)
 
-def write_permutations(template, out_path, mode=None, split=1):
-    if mode == 'aa':
+def all_permutations(template, alphabet=None):
+    """Computes and yields all permutations of the given DNA/protein template in a memory-efficient manner.
+
+    This function automatically detects whether to use the DNA or the protein alphabet depending on the content of the template, unless you specify one or the other using the 'alphabet' parameter. The template supports specifying one possible residue at a position, a set of possible residues, or any residue.
+
+    Parameters:
+    template -- An iterable that specifies the possible values at each position. Each index in the iterable should be one of the following: (1) a character in the DNA/protein alphabet, (2) an iterable of characters, or (3) the '*' character denoting a wildcard.
+    alphabet -- 'aa', 'dna', a different string of characters, or None. If it is not None, the alphabet for permutation will be set to the according language, or to the custom alphabet you specify. If None, the alphabet will be determined automatically.
+
+    Returns:
+    A generator that permutes all possible residues according to the given template.
+
+    Examples:
+    >>> list(all_permutations("A*", alphabet="dna"))
+    ["AA", "AG", "AC", "AT"]
+
+    >>> list(all_permutations(["A", "CG", "*"], alphabet="dna"))
+    ["ACA", "ACG", "ACC", "ACT", "AGA", "AGG", "AGC", "AGT"]
+    """
+    if alphabet == 'aa':
         language = amino_acids
-    elif mode == 'dna':
+    elif alphabet == 'dna':
         language = bases
+    elif alphabet is not None:
+        language = alphabet
     else:
         language = detect_language(template)
 
-    files = []
-    if split > 1:
-        base, ext = os.path.splitext(out_path)
-        for i in range(split):
-            files.append(open(base + '_{}'.format(i) + ext, 'w'))
-    else:
-        files.append(open(out_path, 'w'))
-
-    write_permutations_recursive(template, "", language, files)
-
-    for file in files:
-        file.close()
+    yield from all_permutations_recursive(template, "", language)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Writes all permutations of either DNA or protein sequences with the given template to file.')
-    parser.add_argument('seq', metavar='S', type=str,
-                        help='The template sequence, where * denotes a variable position')
-    parser.add_argument('output', metavar='O', type=str,
-                        help='The path to the output file')
-    parser.add_argument('-m', '--mode', type=str, default=None,
-                        help='The set of characters to permute (aa or dna), optional')
-    parser.add_argument('-s', '--split', type=int, default=1,
-                        help='The number of files to split the output into')
-    args = parser.parse_args()
-
-    write_permutations(args.seq, args.output, args.mode, args.split)
+    pass
