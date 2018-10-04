d4edb0d43783b966c496e7cb8fdd1dc37514af20
venkatesh-sivaraman <venkatesh.sivaraman.98@gmail.com>
2018-10-04 16:06:05 -0400

    Cleaned up repo, keeping only new code for now

--- 03a_countSequences.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-input=$1
-output=$2/seq_summary
-
-mkdir $output
-
-for each in `ls $input`;
-    do sort $input/$each | uniq -c > $output/$each.smry
-done
--- 03b_countSequences.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd dnaframe;
-mkdir summary;
-for each in `ls dnaframe_*`;
-    do sort $each | uniq -c > summary/$each.smry;
-done
